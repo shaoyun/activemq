e9abf67bd98515f8231429b2b8f5582e73bec5be
shaoyun.zhang <shaoyun.zhang@eclicktech.com.cn>
2023-12-11 14:55:55 +0800

    add console support

--- assets/entrypoint/entrypoint/Init.py
+++ assets/entrypoint/entrypoint/Init.py
@@ -246,7 +246,7 @@ class Init():
         self.replace_all(ACTIVEMQ_CONF + "/log4j.properties", "\$\{activemq\.base\}\/data\/", "/var/log/activemq/")
         self.replace_all(ACTIVEMQ_HOME + "/bin/linux-x86-64/wrapper.conf" ,"wrapper\.logfile=%ACTIVEMQ_DATA%\/wrapper\.log", "wrapper.logfile=/var/log/activemq/wrapper.log")
 
-        self.replace_all(ACTIVEMQ_CONF + "/jetty.xml", "<property name=\"host\" value=\"127.0.0.1\"\/>", "<property name=\"host\" value=\"0.0.0.0\"\/>")
+        self.replace_all(ACTIVEMQ_CONF + "/jetty.xml", "<property name=\"host\" value=\"127.0.0.1\"\/>", "<property name=\"host\" value=\"0.0.0.0\" />")
 
 
     def setting_all(self):
